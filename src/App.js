e86d4ab0aad23dc7ba338482373e4a4db185d9e9
Jacky Tang <jackytck@gmail.com>
2018-02-16 14:50:37 +0800

    :sparkles: show manager + num of players + contract balance

--- src/App.js
+++ src/App.js
@@ -2,18 +2,40 @@ import './App.css'
 
 import React, { Component } from 'react'
 
-import logo from './logo.svg'
+import lottery from './lottery'
+import web3 from './web3'
 
 class App extends Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  }
+
+  async componentDidMount () {
+    const [manager, players, balance] = await Promise.all([
+      lottery.methods.manager().call(),
+      lottery.methods.getPlayers().call(),
+      web3.eth.getBalance(lottery.options.address)
+    ])
+
+    this.setState({
+      manager,
+      players,
+      balance
+    })
+  }
+
   render () {
+    const { manager, players, balance } = this.state
+
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {manager}.
+          There are currently {players.length} people entered,
+          copmeting to win {web3.utils.fromWei(balance, 'ether')} Ether!
         </p>
       </div>
     )
